da1ac732e4f19e088ae99b2681a8542a76c53f3b
zhavei <zhavei.vei@gmail.com>
2022-03-22 16:29:00 +0700

    masih di adds

--- app/src/main/java/com/greenTangerangkota/banksampahdlh/MainActivity.kt
+++ app/src/main/java/com/greenTangerangkota/banksampahdlh/MainActivity.kt
@@ -5,17 +5,14 @@ import android.app.ProgressDialog
 import android.content.Intent
 import android.net.ConnectivityManager
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.webkit.ValueCallback
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import android.view.View
+import android.webkit.*
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.ui.AppBarConfiguration
-import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.interstitial.InterstitialAd
 import com.greenTangerangkota.banksampahdlh.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -25,8 +22,8 @@ class MainActivity : AppCompatActivity() {
     private val url = "https://green.tangerangkota.go.id/banksampahdroid/"
     private var currentUrl = ""
     private val FILECHOOSER_RESULTCODE = 190
-    private var mInterstitialAd: InterstitialAd? = null
-    private var interAdRequest: AdRequest? = null
+    //private var mInterstitialAd: InterstitialAd? = null
+    //private var interAdRequest: AdRequest? = null
     private var progressDialog: ProgressDialog? = null
     private var mUploadMessage: ValueCallback<Array<Uri>>? = null
     private var prefManager: PrefManager? = null
@@ -48,25 +45,146 @@ class MainActivity : AppCompatActivity() {
         binding.fab.setOnClickListener {
 
             sendWhatsappMessage(
-                "+628111631631",
+                "+6285715018087",
                 "Halo Admin Bank Sampah Kota Tangerang, Saya admin Bank Sampah : "
             )
         }
-
-
+        if (isOnline()) {
+            binding.contentMain.llError.rlContent.visibility = View.GONE
+            binding.contentMain.webView.visibility = View.VISIBLE
+            startWebView(url)
+        } else {
+            binding.contentMain.llError.rlContent.visibility = View.VISIBLE
+            binding.contentMain.webView.visibility = View.GONE
+            if (progressDialog != null && progressDialog?.isShowing == true)
+                progressDialog?.dismiss()
+        }
     }
 
-    @SuppressLint("SetJavaScriptEnabled")
-    private fun startWebView(url: String){
+    /** private fun loadInterAds() {
+        var interAdRequest = AdRequest.Builder().build()
 
-        binding.contentMain.webView.webViewClient = object : WebViewClient(){
-            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
-                prefManager?.addVar()
-                if (mI)
+        InterstitialAd.load(
+            this, getString(R.string.interstitial_full_screen), interAdRequest,
+            object : InterstitialAdLoadCallback() {
+                override fun onAdFailedToLoad(adError: LoadAdError) {
+                    mInterstitialAd = null
+                    mAdIsLoading = false
+                }
+
+                override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                    mInterstitialAd = interstitialAd
+                    mAdIsLoading = false
+                }
+            }
+        )
+    }
+
+    private fun showInterAds(url: String) {
+        if (mInterstitialAd != null) {
+            mInterstitialAd?.fullScreenContentCallback = object : FullScreenContentCallback() {
+                override fun onAdDismissedFullScreenContent() {
+                    binding.contentMain.webView.loadUrl(url)
+                    loadInterAds()
+                }
+
+                override fun onAdFailedToShowFullScreenContent(adError: AdError?) {
+
+                }
+
+                override fun onAdShowedFullScreenContent() {
+                    mInterstitialAd = null;
+                }
+            }
+            mInterstitialAd?.show(this)
+        } else {
+            binding.contentMain.webView.loadUrl(url)
+        }
+    } **/
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun startWebView(url: String) {
+
+        binding.contentMain.webView.webViewClient = object : WebViewClient() {
+
+//            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+//                prefManager
+//                if (mInterstitialAd != null && prefManager?.tVar?.rem(PrefManager.ADS_SHOW_TIME) == 0) {
+//                    view?.loadUrl(url.toString())
+//                } else{
+//                    view?.loadUrl(url.toString())
+//                }
+//                return true
+//            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                try {
+                    if (progressDialog!!.isShowing) {
+                        progressDialog!!.dismiss()
+                        prefManager!! // here added .addVar
+                        progressDialog = null
+                    }
+                } catch (exception: Exception) {
+                    exception.printStackTrace()
+                }
+            }
+
+            override fun onReceivedHttpError(
+                view: WebView?,
+                request: WebResourceRequest?,
+                errorResponse: WebResourceResponse?
+            ) {
+                super.onReceivedHttpError(view, request, errorResponse)
+                if (progressDialog != null && progressDialog?.isShowing == false) progressDialog?.dismiss()
             }
         }
 
+        binding.contentMain.webView.webChromeClient = object : WebChromeClient() {
+            override fun onShowFileChooser(
+                webView: WebView?,
+                filePathCallback: ValueCallback<Array<Uri>>?,
+                fileChooserParams: FileChooserParams?
+            ): Boolean {
+                mUploadMessage?.onReceiveValue(null)
+                mUploadMessage = filePathCallback
+                val i = Intent(Intent.ACTION_GET_CONTENT)
+                i.addCategory(Intent.CATEGORY_OPENABLE)
+                i.type = "*/*"
+                startActivityForResult(
+                    Intent.createChooser(i, "File Chooser"),
+                    FILECHOOSER_RESULTCODE
+                )
+                return true
+            }
+        }
+        //other webView options
+        binding.contentMain.webView.settings.javaScriptEnabled = true
 
+        //opther webView options
+        binding.contentMain.webView.settings.loadWithOverviewMode = true
+        binding.contentMain.webView.settings.useWideViewPort = true
+        binding.contentMain.webView.scrollBarStyle = WebView.SCROLLBARS_OUTSIDE_OVERLAY
+        binding.contentMain.webView.isScrollbarFadingEnabled = false
+        binding.contentMain.webView.settings.builtInZoomControls = false
+        binding.contentMain.webView.settings.allowContentAccess = true
+
+        //another im added webview options
+        binding.contentMain.webView.settings.setAppCachePath(applicationContext.cacheDir.absolutePath)
+        binding.contentMain.webView.settings.cacheMode = WebSettings.LOAD_DEFAULT
+        binding.contentMain.webView.settings.databaseEnabled = true
+        binding.contentMain.webView.settings.domStorageEnabled = true
+        binding.contentMain.webView.settings.saveFormData = true
+        binding.contentMain.webView.settings.savePassword = true
+        // but seems didnt worked
+
+        binding.contentMain.webView.setDownloadListener(DownloadListener { url, userAgent, contentDisposition, mimeType, contentLength ->
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(url)
+            startActivity(intent)
+        })
+
+        currentUrl = url
+        binding.contentMain.webView.loadUrl(url)
     }
 
     private fun isOnline(): Boolean{
