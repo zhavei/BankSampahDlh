c2054477e3965a01d65d41756734ac0b5c539f17
zhavei <zhavei.vei@gmail.com>
2022-03-22 22:22:16 +0700

    still playing with sharedPreference

--- app/src/main/java/com/greenTangerangkota/banksampahdlh/PrefManager.kt
+++ app/src/main/java/com/greenTangerangkota/banksampahdlh/PrefManager.kt
@@ -5,27 +5,27 @@ import android.content.SharedPreferences
 
 class PrefManager internal constructor(mContext: Context) {
     var editor: SharedPreferences.Editor? = null
-    fun addVar() {
-        if (pref != null) {
-            var tVar = pref!!.getInt("XD", 0)
-            editor = pref!!.edit()
-            editor?.putInt("XD",++tVar)
-            editor?.commit()
-        }
-    }
+//    fun addVar() {
+//        if (pref != null) {
+//            var tVar = pref!!.getInt("XD", 0)
+//            editor = pref!!.edit()
+//            editor?.putInt("XD",++tVar)
+//            editor?.commit()
+//        }
+//    }
 
-    val tVar: Int
-        get() {
-            var tVar = 0
-            if (pref != null) {
-                tVar = pref!!.getInt("XD", 0)
-            }
-            return tVar
-        }
+//    val tVar: Int
+//        get() {
+//            var tVar = 0
+//            if (pref != null) {
+//                tVar = pref!!.getInt("XD", 0)
+//            }
+//            return tVar
+//        }
 
     companion object {
         var pref: SharedPreferences? = null
-        const val ADS_SHOW_TIME = 10
+       // const val ADS_SHOW_TIME = 10
     }
 
     init {
